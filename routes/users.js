2f0011d7d622d2a0f0791a45dc0ae0377bf57325
Vedansh <vedanshsugandhi@gmail.com>
2023-11-06 17:18:18 +0530

    removed files

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const middleware = require('../common/middleware')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
+  
+});
+
+router.get('/register', function(req, res, next) {
+  res.send('User Registration Successful');
+});
+
+router.get('/logout', function(req, res, next) {
+  res.send('Logout Successful');
 });
 
 module.exports = router;
